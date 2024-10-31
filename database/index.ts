5e50835acb1abe872c9f2d6f707bf5f1c1481e12
nishattasnim3646 <nishattasnim3646@gmail.com>
2024-10-31 15:55:33 +0600

    added payment table

--- database/index.ts
+++ database/index.ts
@@ -5,6 +5,7 @@ import { ColumnType, Generated, JSONColumnType, Selectable, Insertable, Updateab
 
 // Define the complete Database schema for Kysely
 export interface DB {
+  payment:payement 
   address: Address;
   auth_session: AuthSession;
   product: Product;
